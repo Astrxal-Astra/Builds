df5a90332559f20a0f834536b6f17b2a3e283abb
Astrxal-Astra <ivanboncan020@gmail.com>
2026-08-09 20:26:33 +0800

    Terminal-Chatbot: Gemini
    ##Features: Terminal-based Gemini chat, dynamic model selection, streaming responses, error handling.
    Known Bugs: Paste-triggered carriage return execution causing input duplication on Pydroid.
    Requirements: Python 3.10+, google-genai.

---
+++ CLI_LLM_Gemini.py
@@ -0,0 +1,32 @@
+from google import genai
+from google.genai.errors import APIError
+
+print("[From aistudio.google.com]")
+api_key = input("Paste your API HERE: ")
+client = genai.Client(api_key=api_key)
+
+models = ["gemini-3.6-flash", "gemini-3.5-flash", "gemini-3.5-flash-lite", "gemini-3.1-pro-preview", "gemini-3.1-pro-preview-customtools", "gemini-3.1-flash-lite", "gemini-3.1-flash-lite-preview", "gemini-3-pro-preview", "gemini-3-flash-preview", "gemini-2.5-pro", "gemini-2.5-flash", "gemini-2.5-flash-lite", "gemini-2.0-flash", "gemini-2.0-flash-001", "gemini-2.0-flash-lite", "gemini-2.0-flash-lite-001", "gemini-flash-latest", "gemini-flash-lite-latest", "gemini-pro-latest", "gemma-4-31b-it", "gemma-4-26b-a4b-it", "gemini-omni-flash-preview"]
+print("SELECT Model Options: ", *models, sep="\n\t")
+Choice = input("Enter the Gemini model's name u want to use: ")
+
+chat = client.chats.create(model=Choice)
+
+print("\n--- Chat STARTED TYPE {exit} to well.. exit of course ---")
+
+while True:
+    user_input = input("\nYou: ")
+    if user_input.lower() == "exit":
+        break
+
+    try:
+        response = chat.send_message_stream(user_input)
+        print("Gemini: ", end="", flush=True)
+        for chunk in response:
+        	if chunk.text:
+        		print(chunk.text, end="", flush=True)
+        print()
+    except APIError as e:
+        print(f"\nGemini: API error -> {e}")
+    except Exception as i:
+        print(f"\nGemini: Unexpected error -> {i} ")
+        
