be636f23c9f80ab6489c3ea6a6f6e4140c04d1bf
Astrxal-Astra <ivanboncan020@gmail.com>
2026-08-15 13:57:58 +0800

    Enhance model selection and prompt handling

--- CLI_LLM_Gemini.py
+++ CLI_LLM_Gemini.py
@@ -1,15 +1,24 @@
 from google import genai
 from google.genai.errors import APIError
+from google.genai import types
 
 print("[From aistudio.google.com]")
-api_key = input("Paste your API HERE: ")
+api_key = input("\nPaste your API HERE: ")
 client = genai.Client(api_key=api_key)
 
 models = ["gemini-3.6-flash", "gemini-3.5-flash", "gemini-3.5-flash-lite", "gemini-3.1-pro-preview", "gemini-3.1-pro-preview-customtools", "gemini-3.1-flash-lite", "gemini-3.1-flash-lite-preview", "gemini-3-pro-preview", "gemini-3-flash-preview", "gemini-2.5-pro", "gemini-2.5-flash", "gemini-2.5-flash-lite", "gemini-2.0-flash", "gemini-2.0-flash-001", "gemini-2.0-flash-lite", "gemini-2.0-flash-lite-001", "gemini-flash-latest", "gemini-flash-lite-latest", "gemini-pro-latest", "gemma-4-31b-it", "gemma-4-26b-a4b-it", "gemini-omni-flash-preview"]
 print("SELECT Model Options: ", *models, sep="\n\t")
-Choice = input("Enter the Gemini model's name u want to use: ")
 
-chat = client.chats.create(model=Choice)
+while True:
+     Choice = input("Enter the Gemini model's name u want to use: ")
+     prompt = input("\nEnter prompt u wanna give gemini [example it's personality, how to act, etc... NO legal stuff though] type ""skip"" to skip: ")
+     if prompt.lower() == "skip":
+     	config = None 
+     else:
+     		config = types.GenerateContentConfig(system_instruction=prompt)
+     		break
+
+chat = client.chats.create(model=Choice, config=config)
 
 print("\n--- Chat STARTED TYPE {exit} to well.. exit of course ---")
 
