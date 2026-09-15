c9db1fb35ef1397ec2e20ea9e2d80a69248a02b8
Astrxal <ivanboncan020@gmail.com>
2026-09-15 11:39:23 +0800

    Enhance logging and error handling in Webhook.py

--- Webhook.py
+++ Webhook.py
@@ -2,21 +2,27 @@ import requests
 import validators
 import time
 import sys
+import logging
+
+logging.basicConfig(filename="discord.script", filemode="a", level=logging.INFO, encoding="utf-8", format="%(asctime)s - [%(name)s] - %(levelname)s - %(message)s")
+logger = logging.getLogger("DISCORD-WEBHOOK")
 
 while True:    
     try:
         webhook_url = input("Enter discord webhook URL: ").strip()
         if validators.url(webhook_url):
             break
-        print(f"Invalid URL {webhook_url}")
-        print("Try again")
+        print(f"Invalid URL: ['{webhook_url}']", "\nTry Again")
+
+        logging.error(f"invalid URL: {webhook_url}")
     except KeyboardInterrupt:
         print("\nTyping Interrupted")
-        sys.exit()
+        logging.error("Program stopped during URL input via KeyboardInterrupt.")
+        sys.exit(1)
         
 while True:
     try:
-        time.sleep(3)
+        time.sleep(2)
         message = input("\nEnter Message to send through webhook[type: exit ; to exit]: ").strip()
         if message.lower() == "exit":
             break
@@ -25,14 +31,18 @@ while True:
         try:
             response = requests.post(webhook_url, json=data)
             if response.status_code == 204:
-                print("\nSent")
+                print("\n---Sent---")
+                logging.info("message sent")
             else:
                 print(f"Not working: {response.status_code}")
                 print(response.text)
+                logging.error(f"discord rejected the message. Status: {response.status_code}")
         except requests.exceptions.RequestException:
-            print(f"Failed to send: {message}")
-            print("NO INTERNET, or Discord Servers are down (TRY AGAIN LATER)")
+            print(f"Failed to send: {message}", "\nNO INTERNET, or Discord Servers are down (TRY AGAIN LATER)")                       
+            logger.critical(f"couldn't send {message} due to unstable connection")
 
     except KeyboardInterrupt:
-        print("\nExiting...")
-        sys.exit()
+        print("\nExiting program...")
+        logging.error("typing interrupted")
+        sys.exit(1)
+        
